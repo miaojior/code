4b7ea5c877b01a54e0551b39cf6dc6509f5f649e
miaojior <gongmingyang123456@outlook.com>
2023-03-31 21:52:26 +0800

    code

--- python/2.py
+++ python/2.py
@@ -1,34 +1,26 @@
-# 导入matplotlib库
-import matplotlib.pyplot as plt
-# 导入math库
-import math
+# 导入turtle库
+import turtle
 
-# 定义边长和线宽
-side = 200
-width = 10
+# 设置画笔的颜色和宽度
+turtle.color("green")
+turtle.pensize(10)
 
-# 定义五个顶点的坐标
-x1 = 0
-y1 = 0
-x2 = side
-y2 = 0
-x3 = side + side * math.cos(math.radians(72))
-y3 = side * math.sin(math.radians(72))
-x4 = side / 2
-y4 = side * math.sin(math.radians(72)) + side * math.sin(math.radians(36))
-x5 = -side * math.cos(math.radians(72))
-y5 = y3
+# 设置填充的颜色
+turtle.fillcolor("red")
 
-# 创建一个多边形对象
-polygon = plt.Polygon([[x1, y1], [x2, y2], [x3, y3], [x4, y4], [x5, y5]],
-                      facecolor='red', edgecolor='green', linewidth=width)
+# 开始填充
+turtle.begin_fill()
 
-# 将多边形添加到当前的图形中
-plt.gca().add_patch(polygon)
+# 画一个正五边形，每个边长为200，每个角为72度
+for i in range(5):
+    turtle.forward(200)
+    turtle.left(72)
 
-# 设置坐标轴的范围和比例
-plt.axis([-100, 400, -100, 400])
-plt.axis('equal')
+# 结束填充
+turtle.end_fill()
+
+# 隐藏海龟
+turtle.hideturtle()
 
 # 显示图形
-plt.show()
+turtle.done()
--- python/6.py
+++ python/6.py
@@ -1,1 +1,5 @@
-for i in range (1,)
+for i in range (1,12):
+    if i==1 or i==6 or i==11:#第一行第六行第十一行画横线
+        print("+ - - - - + - - - - +\n")#print自动向控制台打印一行 回车的转义字符\n可以加一行实现竖线的间隔
+    else :
+        print("|         |         |\n")
