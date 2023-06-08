035aacb44785265b5b289c3c2b838842b02a6a24
miaojior <gongmingyang123456@outlook.com>
2023-06-08 20:39:49 +0800

    code

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,8 +1,9 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
-using namespace std;
+int c[3][]={{1,2},{1,2,3},{1,2,3,4}};
 
 int main(){
-    int a=1,b=2,c=3;
-    cout<<(a<=b<=c)<<endl;
+    for(int i=0;i<2;i++)
+    for(int j=0;j<3;j++)
+    std::cout<<c[i][j];
 }
