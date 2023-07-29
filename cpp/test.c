a1271448c01a4f1aad75d1cd44814a880cd86ce8
miaojior <gongmingyang123456@outlook.com>
2023-07-29 09:45:52 +0800

    code

--- cpp/test.c
+++ cpp/test.c
@@ -1,9 +1,6 @@
-#include <stdio.h>
-
-int main(){
-    int (*p)[10];
-    int a[20]={1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0};
-    p=&a[0];
-    printf("%d",(*p)[11]);
-    return 0;
-} 
+#include<stdio.h>
+int main()
+{
+       int a=9;
+       int b[a];	
+}
