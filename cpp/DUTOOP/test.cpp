af169d50698157a16c0d22cb9f3e27b3a3eae2c2
miaojior <gongmingyang123456@outlook.com>
2024-02-06 15:24:11 +0800

    backup

---
+++ cpp/DUTOOP/test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"Hello iPad"<<std::endl;
+    return 0;
+}
--- cpp/test.cpp
+++ cpp/test.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 
 int main(){
-cout<<"hello world"<<endl;
-return 0;
+    cout<<"hello ipad"<<endl;
+    return 0;
 }
