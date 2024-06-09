b550782f0a07672e2f861eb3d4f97281a6e567b8
miaojior <gongmingyang123456@outlook.com>
2024-06-09 19:13:26 +0800

    backup

--- cpp/DUTOOP/21.cpp
+++ cpp/DUTOOP/21.cpp
@@ -1,70 +1,67 @@
 #include <iostream>
-#include <queue>
 #include <cstring>
 using std::cin; using std::cout; using std::endl;
-using std::queue;
-
-struct Point {
-    int x, y;
-};
 
 class Island {
 private:
+    int grid[9][9];
     int n;
-    int matrix[9][9];
-    bool visited[9][9];
-    bool isSurrounded(int x, int y);
+    bool isSurroundedByOnes(int x, int y);
 public:
     Island(int size);
-    void outputResult();
+    void readGrid();
+    int calculateIslandArea();
+    void outputIslandArea();
 };
 
 int main() {
-    int size;
-    cin >> size;
-    Island island(size);
-    island.outputResult();
+    int n;
+    cin >> n;
+
+    Island island(n);
+    island.readGrid();
+    island.outputIslandArea();
+
     return 0;
 }
 
-Island::Island(int size) : n(size){
+bool Island::isSurroundedByOnes(int x, int y) {
+    for (int i = x + 1; i < n; i++) {
+        if (grid[i][y] == 1) break;
+        if (grid[i][y] == 0 && i == n - 1) return false;
+    }
+    for (int i = x - 1; i >= 0; i--) {
+        if (grid[i][y] == 1) break;
+        if (grid[i][y] == 0 && i == 0) return false;
+    }
+    for (int j = y + 1; j < n; j++) {
+        if (grid[x][j] == 1) break;
+        if (grid[x][j] == 0 && j == n - 1) return false;
+    }
+    for (int j = y - 1; j >= 0; j--) {
+        if (grid[x][j] == 1) break;
+        if (grid[x][j] == 0 && j == 0) return false;
+    }
+    return true;
+}
+
+Island::Island(int size) : n(size) {}
+
+void Island::readGrid() {
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
-            cin >> matrix[i][j];
-    memset(visited, false, sizeof(visited));
+            cin >> grid[i][j];
 }
 
-bool Island::isSurrounded(int x, int y) {
-    queue<Point> q;
-    q.push({x, y});
-    visited[x][y] = true;
-    bool surrounded = true;
-    int dirs[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    while (!q.empty()){
-        Point current = q.front();
-        q.pop();
-        for (int i = 0; i < 4; ++i) {
-            int nx = current.x + dirs[i][0];
-            int ny = current.y + dirs[i][1];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                if (!visited[nx][ny] && matrix[nx][ny] == 0) {
-                        q.push({nx, ny});
-                        visited[nx][ny] = true;
-                    }
-            }else{
-                surrounded = false;
-            }
-        }
-    }
-    return surrounded;
+int Island::calculateIslandArea() {
+    int islandArea = 0;
+    for (int i = 1; i < n - 1; ++i)
+        for (int j = 1; j < n - 1; ++j)
+            if (grid[i][j] == 0 && isSurroundedByOnes(i, j))
+                ++islandArea;
+    return islandArea;
 }
 
-void Island::outputResult(){
-    int area = 0;
-    for (int i = 1; i < n - 1; ++i) 
-        for (int j = 1; j < n - 1; ++j) 
-            if (matrix[i][j] == 0 && !visited[i][j]) 
-                if (isSurrounded(i, j)) 
-                    area++;
-    cout << area << endl;
+void Island::outputIslandArea() {
+    cout << calculateIslandArea() << endl;
 }
